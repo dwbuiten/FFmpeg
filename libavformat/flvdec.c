5cada70112c8e3ccc5215c00a474cc7b47e2d59f
Anton Khirnov <anton@khirnov.net>
2015-10-11 11:08:24 +0200

    flvdec: do not create any streams in read_header()
    The current muxer behaviour is to create streams in read_header() based
    on the audio/video presence flags, but fill in the stream parameters
    later when we actually get some packets for them. This is rather shady,
    since other demuxers set the stream parameters immediately when the
    stream is created and do not touch the stream codec context after that.
    
    Change the flv demuxer to behave in the same way as other similar
    demuxers -- create the streams only when we get a packet for them.

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -629,22 +629,13 @@ static int flv_read_metabody(AVFormatContext *s, int64_t next_pos)
 
 static int flv_read_header(AVFormatContext *s)
 {
-    int offset, flags;
     FLVContext *flv = s->priv_data;
+    int offset;
 
     avio_skip(s->pb, 4);
-    flags = avio_r8(s->pb);
+    avio_r8(s->pb); // flags
 
     s->ctx_flags |= AVFMTCTX_NOHEADER;
-
-    if (flags & FLV_HEADER_FLAG_HASVIDEO)
-        if (!create_stream(s, AVMEDIA_TYPE_VIDEO))
-            return AVERROR(ENOMEM);
-    if (flags & FLV_HEADER_FLAG_HASAUDIO)
-        if (!create_stream(s, AVMEDIA_TYPE_AUDIO))
-            return AVERROR(ENOMEM);
-    // Flag doesn't indicate whether or not there is script-data present. Must
-    // create that stream if it's encountered.
 
     offset = avio_rb32(s->pb);
     avio_seek(s->pb, offset, SEEK_SET);
